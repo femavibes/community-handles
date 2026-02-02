2d294c5f3bce91a65668f19a0f2461a66e722148
femavibes <your-email@example.com>
2026-02-02 01:54:48 +0000

    fix script loading for ko-fi widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata, type Viewport } from "next"
+import Script from "next/script"
 
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
@@ -30,9 +31,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head>
-          <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
-        </head>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
@@ -45,16 +43,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
             </div>
             <TailwindIndicator />
           </ThemeProvider>
-          <script dangerouslySetInnerHTML={{
-            __html: `
+          <Script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js" />
+          <Script id="kofi-widget" strategy="lazyOnload">
+            {`
               kofiWidgetOverlay.draw('urbanismplus', {
                 'type': 'floating-chat',
                 'floating-chat.donateButton.text': 'Support',
                 'floating-chat.donateButton.background-color': '#00b9fe',
                 'floating-chat.donateButton.text-color': '#fff'
               });
-            `
-          }} />
+            `}
+          </Script>
         </body>
       </html>
     </>
